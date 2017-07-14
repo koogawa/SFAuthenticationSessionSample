e029973f573873661920610200a8187bdf4f8e3a
Kosuke Ogawa <ogawa_kousuke@aratana.jp>
2017-07-14 18:49:35 +0900

    Add files

---
+++ SFAuthenticationSessionSample/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  SFAuthenticationSessionSample
+//
+//  Created by ogawa_kousuke on 2017/07/14.
+//  Copyright © 2017年 aratana inc. All rights reserved.
+//
+
+import UIKit
+import SafariServices
+
+class ViewController: UIViewController {
+
+    var session: SFAuthenticationSession? = nil
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func auth(_ sender: Any) {
+        let url = URL(string: "https://ja.foursquare.com/oauth2/authenticate?client_id=(YOUR_CLIENT_ID)&response_type=token&redirect_uri=fsoauthexample://authorized")!
+        self.session = SFAuthenticationSession(url: url, callbackURLScheme: nil) { url, error in
+            print(url, error)
+            // fsoauthexample://authorized#access_token=XXXXXXXXXXXXXX
+        }
+        session?.start()
+    }
+
+}
+
