5743acaff4c3d79acda51116820c5912372527f1
Aaron Brager <getagb@gmail.com>
2017-07-22 18:19:27 -0500

    Deleted some view controller clutter

--- SFAuthenticationSessionSample/ViewController.swift
+++ SFAuthenticationSessionSample/ViewController.swift
@@ -13,16 +13,6 @@ class ViewController: UIViewController {
 
     var session: SFAuthenticationSession? = nil
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
     @IBAction func auth(_ sender: Any) {
         let url = URL(string: "https://ja.foursquare.com/oauth2/authenticate?client_id=(YOUR_CLIENT_ID)&response_type=token&redirect_uri=fsoauthexample://authorized")!
         self.session = SFAuthenticationSession(url: url, callbackURLScheme: nil) { url, error in
